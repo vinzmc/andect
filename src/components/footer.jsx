24add84303fa4ff4c15a4a00fa72fec975c092cc
vinzmc <maurice.marvin@student.umn.ac.id>
2021-11-09 15:24:23 +0700

    fixing grammar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="bg-gray-800 ">
             <div className="container mx-auto p-2 flex">
                 <div className="flex-auto text-gray-500 text-sm text-center">
-                    <NavLink to="/andect/contributor"><p>Created by Maurice and Teams. <span>&copy;</span> 2021</p></NavLink>
+                    <NavLink to="/andect/contributor"><p>Created by Maurice and Team. <span>&copy;</span> 2021</p></NavLink>
                 </div>
             </div>
         </div>
