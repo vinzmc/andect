53f289f525bbfddfdd0d72fac44ff6d5dc778b4f
vinzmc <maurice.marvin@student.umn.ac.id>
2021-11-14 22:10:08 +0700

    improve mobile responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 //dependency
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 //dependency
 import { NavLink } from 'react-router-dom';
@@ -9,6 +10,22 @@ import logo from '../assets/logo/ANDECT.png';
 import githubLogo from '../assets/github.png'
 
 export default function Navbar() {
+    const [menu, setMenu] = useState(true);
+    const isLarge = useMediaQuery({ query: `(min-width: 1024px)` });
+    const menuOnClick = () => {
+        if (menu) {
+            setMenu(false);
+        } else {
+            setMenu(true);
+        }
+    }
+
+    useEffect(() => {
+        if(isLarge){
+            setMenu(true);
+        }
+    }, [isLarge]);
+
     return (
         <nav className="container mx-auto p-2 flex items-center justify-between flex-wrap bg-white">
             {/* Website icon and name*/}
@@ -18,10 +35,9 @@ export default function Navbar() {
                     <span className="font-semibold text-4xl tracking-tight">ANDECT</span>
                 </button>
             </NavLink>
-
             {/* Menu button for small width (unfinished) */}
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded border-yellow-400 hover:text-yellow-700 hover:border-yellow-600">
+                <button className="flex items-center px-3 py-2 border rounded border-yellow-400 hover:text-yellow-700 hover:border-yellow-600" onClick={menuOnClick}>
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <title>Menu</title>
                         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -31,22 +47,25 @@ export default function Navbar() {
 
             {/* List of links */}
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div className="text-xl font-semibold lg:flex-grow">
-                    <NavLink to="/" className={"block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 mr-5 rounded align-middle"}>
-                        Home
-                    </NavLink >
-                    <NavLink to="/test" className="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 mr-5 rounded align-middle">
-                        Test
-                    </NavLink >
-                    <NavLink to="/contributor" className="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 rounded align-middle">
-                        Contributor
-                    </NavLink >
-                </div>
+                {menu &&
+                    <div className="text-xl font-semibold lg:flex-grow">
+                        <NavLink to="/" className={"block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 mr-5 rounded align-middle"}>
+                            Home
+                        </NavLink >
+                        <NavLink to="/test" className="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 mr-5 rounded align-middle">
+                            Test
+                        </NavLink >
+                        <NavLink to="/contributor" className="block mt-4 lg:inline-block lg:mt-0 hover:text-yellow-600 hover:bg-gray-50 p-2 rounded align-middle">
+                            Contributor
+                        </NavLink >
+                    </div>
+                }
+
 
                 {/* GitHub Project Link */}
                 <div>
                     <a href="https://github.com/vinzmc/andect" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
-                        <img src={githubLogo} className="w-12 align-middle hover:shadow-inner p-2 rounded" alt="Project Link"/>
+                        <img src={githubLogo} className="w-12 align-middle hover:shadow-inner p-2 rounded" alt="Project Link" />
                     </a>
                 </div>
             </div>
